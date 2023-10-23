8f9e88790d3822a5c2c040b690abf097ba012659
omarotierra <2omarhernandez2@gmail.com>
2023-10-23 04:16:51 -0600

    create linear-regression

--- index.js
+++ index.js
@@ -39,4 +39,20 @@ app.get("/size", (req, res, next) => {
   res.sendfile("assets/size.json");
 });
 
+app.get("/3a_test1", (req, res, next) => {
+  res.sendfile("assets/3a_test1.json");
+});
+
+app.get("/3a_test2", (req, res, next) => {
+  res.sendfile("assets/3a_test2.json");
+});
+
+app.get("/3a_test3", (req, res, next) => {
+  res.sendfile("assets/3a_test3.json");
+});
+
+app.get("/3a_test4", (req, res, next) => {
+  res.sendfile("assets/3a_test4.json");
+});
+
 app.listen(port, () => console.log("listening on port " + port));
